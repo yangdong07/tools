724f68606bf1e02caa6f21ceb78cc7d0c91f0a36
sparrowlc <yangdong07@gmail.com>
2018-01-28 00:01:54 +0800

    fix database; add make txt function

--- spider/novel.py
+++ spider/novel.py
@@ -20,34 +20,64 @@ headers = {
 }
 
 
-page_url = start_url
+def crawl(start_url):
+    page_url = start_url
+    count = 0
 
-count = 0
+    while count < 5000:
+        count += 1
+        resp = requests.get(page_url)
+        soup = BeautifulSoup(resp.content, 'lxml')
 
-while count < 5000:
-    count += 1
-    resp = requests.get(page_url)
-    soup = BeautifulSoup(resp.content, 'html.parser')
+        try:
+            chapter = soup.find('div', class_='content')
+            title = chapter.find('h1').get_text()
+            content = chapter.find(id='content').get_text()
 
-    try:
-        chapter = soup.find('div', class_='content')
-        title = chapter.find('h1').get_text()
-        content = chapter.find(id='content').get_text()
+            # 从全局找一下，比从局部找容错率高
+            next_page = soup.find('a', text='下一章').get('href')
+            next_page_url = urljoin(start_url, next_page)
 
-        # 从全局找一下，比从局部找容错率高
-        next_page = soup.find('a', text='下一章').get('href')
-        next_page_url = urljoin(start_url, next_page)
+            data = {
+                'name': name,
+                'author': author,
+                'count': count,
+                'title': title,
+                'content': '\n'.join(content.split()),
+                'url': page_url,
+                'next_page_url': next_page_url,
+            }
+            collection.insert(data)
+            page_url = next_page_url
 
-        data = {
-            'count': count,
-            'title': title,
-            'content': '\n'.join(content.split()),
-            'url': page_url,
-            'next_page_url': next_page_url,
-        }
-        collection.insert(data)
-        page_url = next_page_url
+        except AttributeError as e:
+            print(count, page_url)
+            break
 
-    except AttributeError as e:
-        print(count, page_url)
-        break
+
+def fix():
+    for doc in collection.find({'content': ''}):
+        print(doc['url'])
+        resp = requests.get(doc['url'])
+        soup = BeautifulSoup(resp.content, 'lxml')
+        content = soup.find(id='content').get_text()
+        doc.update(content=content)
+        collection.replace_one({'_id': doc['_id']}, doc)
+
+    # 第一次缺少书名和作者名作为index，添加上
+    # collection.update_many({}, {'$set': {'name': '我要做皇帝', 'author': '要离刺荆轲'}})
+    # collection.create_index({'name': 1, 'author': 1})
+
+
+def make_txt(name):
+    file_name = name + '.txt'
+    with open(file_name, 'w') as f:
+        f.write(file_name + '\n')
+        # 找出全部，按count排序
+        for doc in collection.find({'name': name}).sort('count'):
+            f.write('\n\n' + doc['title'] + '\n\n')
+            f.write(doc['content'])
+
+
+if __name__ == '__main__':
+    make_txt('我要做皇帝')
